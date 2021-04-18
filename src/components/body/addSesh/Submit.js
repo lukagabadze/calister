7434dfd4022c346b7e782b5aaf391ed58fd8488f
lukagabadze <gabadzeluka2003@gmail.com>
2021-04-18 20:00:44 +0400

    submit button hover bug fix

--- src/components/body/addSesh/Submit.js
+++ src/components/body/addSesh/Submit.js
@@ -4,7 +4,7 @@ function Submit(props) {
   return (
     <button
       type="submit"
-      className="w-full h-full text-lg py-1 bg-gray-300 text-black hover:bg-gray-400 hover:text-white duration-100"
+      className="w-full h-full text-lg py-1 rounded-b-md bg-gray-300 text-black hover:bg-gray-400 hover:text-white duration-100"
     >
       Submit
     </button>
